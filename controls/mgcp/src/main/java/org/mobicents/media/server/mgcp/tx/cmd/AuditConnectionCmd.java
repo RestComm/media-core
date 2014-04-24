fa206e6609ef9b240b3e0244b216d3204248da16
Henrique Rosa <million@henriqusmacbook.lan>
2014-04-24 18:53:37 +0100

    MEDIA-22 #comment Fixed minor issues on AUCX command

--- controls/mgcp/src/main/java/org/mobicents/media/server/mgcp/tx/cmd/AuditConnectionCmd.java
+++ controls/mgcp/src/main/java/org/mobicents/media/server/mgcp/tx/cmd/AuditConnectionCmd.java
@@ -43,9 +43,8 @@ public class AuditConnectionCmd extends Action {
 	private TaskChain handler;
 	
 	// Endpoint identifier
-	private Text localName = new Text();
-	private Text domainName = new Text();
-	private Text[] endpointName = new Text[] { localName, domainName };
+	private Text localName = new Text("");
+	private Text domainName = new Text("");
 	private MgcpEndpoint[] endpoints = new MgcpEndpoint[1];
 	private MgcpEndpoint endpoint;
 
@@ -60,7 +59,7 @@ public class AuditConnectionCmd extends Action {
 	 * The <b>EndpointId</b> parameter specifies the endpoint that handles the
 	 * connection. The wildcard conventions SHALL NOT be used.
 	 */
-	private Parameter endpointId;
+	private Text[] endpointName = new Text[] { localName, domainName };
 	/**
 	 * The (possibly empty) <b>RequestedInfo</b> describes the information that
 	 * is requested for the ConnectionId within the EndpointId specified. The
@@ -112,16 +111,14 @@ public class AuditConnectionCmd extends Action {
 		public long perform() {
 			// Get parameters from the MGCP request
 			request = (MgcpRequest) getEvent().getMessage();
-			endpointId = request.getParameter(Parameter.ENDPOINT_ID);
 			connectionId = request.getParameter(Parameter.CONNECTION_ID);
 			requestedInfo = request.getParameter(Parameter.REQUESTED_INFO);
 			
 			// Validate the parameters
-			if(endpointId == null) {
+			if(request.getEndpoint() == null || request.getEndpoint().length() == 0) {
 				throw new MgcpCommandException(MgcpResponseCode.PROTOCOL_ERROR, ENDPOINT_ID_MISSING);
 			} else {
-				//getting endpoint name via existing method
-	            request.getEndpoint().divide('@', endpointName);
+				request.getEndpoint().divide('@', endpointName);
 	            // TODO endpoint id SHALL NOT use wildcard conventions
 			}
 			
@@ -171,18 +168,18 @@ public class AuditConnectionCmd extends Action {
 				} else if (param.equals(Parameter.LOCAL_CONNECTION_OPTIONS)) {
 					queryLocalConnectionOpts = true;
 					// TODO hrosa - get local connection options
-					localConnectionOpts = new Text();
+					localConnectionOpts = new Text("");
 				} else if (param.equals(Parameter.MODE)) {
 					queryMode = true;
 					connectionMode = connection.getConnection().getMode();
 				} else if (param.equals(Parameter.REMOTE_CONNECTION_DESCRIPTION)) {
 					queryRemoteConnectionDes = true;
 					String remoteSdp = connection.getConnection().getRemoteDescriptor();
-					remoteConnectionDes = remoteSdp == null ? new Text() : new Text(remoteSdp);
+					remoteConnectionDes = remoteSdp == null ? new Text("v=0") : new Text(remoteSdp);
 				} else if (param.equals(Parameter.LOCAL_CONNECTION_DESCRIPTION)) {
 					queryLocalConnectionDes = true;
 					String localSdp = connection.getConnection().getLocalDescriptor();
-					localConnectionDes = localSdp == null ? new Text() : new Text(localSdp);
+					localConnectionDes = localSdp == null ? new Text("v=0") : new Text(localSdp);
 				} else if (param.equals(Parameter.CONNECTION_PARAMETERS)) {
 					queryConnectionParams = true;
 					auditConnectionParameters(connection);
@@ -221,27 +218,28 @@ public class AuditConnectionCmd extends Action {
             response.setTxID(transaction().getId());
 
 			/*
+			 * The AuditConnection response will in turn include information
+			 * about each of the items auditing info was requested for.
+			 * 
 			 * If no info was requested and the EndpointId is valid, the gateway
 			 * simply checks that the connection exists, and if so returns a
 			 * positive acknowledgement.
-			 */
-            response.setParameter(Parameter.CONNECTION_ID, connectionId.getValue());
-
-			/*
-			 * The AuditConnection response will in turn include information
-			 * about each of the items auditing info was requested for
 			 */
 			if(requestedInfo != null) {
 				if(queryCallId) {
 					response.setParameter(Parameter.CALL_ID, new Text(callId));
 				}
 				if(queryNotifiedEntity) {
-					Text entity = notifiedEntity == null ? new Text() : notifiedEntity.getValue();
+					Text entity = new Text("");
+					if(notifiedEntity != null) {
+						notifiedEntity.getValue().copy(entity);
+						entity.trim();
+					}
 					response.setParameter(Parameter.NOTIFIED_ENTITY, entity);
 				}
 				if(queryLocalConnectionOpts) {
 					// TODO Add local connection options to response
-					response.setParameter(Parameter.LOCAL_CONNECTION_OPTIONS, new Text());
+					response.setParameter(Parameter.LOCAL_CONNECTION_OPTIONS, localConnectionOpts);
 				}
 				if(queryMode) {
 					response.setParameter(Parameter.MODE, connectionMode.getDescription());
