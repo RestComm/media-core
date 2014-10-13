6e8c102481b6d50c311067b5a969666ba7ba07e9
Henrique Rosa <henrique.rosa@telestax.com>
2014-10-13 13:33:11 +0100

    Adding tests for SSRC Generator.

--- io/rtp/src/main/java/org/mobicents/media/server/impl/rtp/SsrcGenerator.java
+++ io/rtp/src/main/java/org/mobicents/media/server/impl/rtp/SsrcGenerator.java
@@ -3,11 +3,14 @@ package org.mobicents.media.server.impl.rtp;
 import java.util.UUID;
 
 /**
+ * Generates SSRC identifiers for an RTP Channel.
  * 
- * @author Henrique Rosa
- *
+ * @author Henrique Rosa (henrique.rosa@telestax.com)
+ * 
  */
 public class SsrcGenerator {
+
+	public static final int MAX_SIZE = 32;
 
 	public static long generateSsrc() {
 		UUID uuid = UUID.randomUUID();
@@ -15,29 +18,34 @@ public class SsrcGenerator {
 		byte[] data = uIntLongToByteWord(leastSignificantBits);
 		return bytesToUIntLong(data, 0);
 	}
-	
-	/** 
-	 * Combines four bytes (most significant bit first) into a 32 bit unsigned integer.
+
+	/**
+	 * Combines four bytes (most significant bit first) into a 32 bit unsigned
+	 * integer.
 	 * 
 	 * @param bytes
-	 * @param index of most significant byte
+	 * @param index
+	 *            of most significant byte
 	 * @return long with the 32 bit unsigned integer
 	 */
-	 static long bytesToUIntLong(byte[] bytes, int index) {
+	static long bytesToUIntLong(byte[] bytes, int index) {
 		long accum = 0;
 		int i = 3;
-		for (int shiftBy = 0; shiftBy < 32; shiftBy += 8 ) {
-			accum |= ( (long)( bytes[index + i] & 0xff ) ) << shiftBy;
+		for (int shiftBy = 0; shiftBy < 32; shiftBy += 8) {
+			accum |= ((long) (bytes[index + i] & 0xff)) << shiftBy;
 			i--;
 		}
 		return accum;
 	}
-	
-	/** 
-	 * Converts an unsigned 32 bit integer, stored in a long, into an array of bytes.
+
+	/**
+	 * Converts an unsigned 32 bit integer, stored in a long, into an array of
+	 * bytes.
 	 * 
-	 * @param j a long
-	 * @return byte[4] representing the unsigned integer, most significant bit first. 
+	 * @param j
+	 *            a long
+	 * @return byte[4] representing the unsigned integer, most significant bit
+	 *         first.
 	 */
 	static byte[] uIntLongToByteWord(long j) {
 		int i = (int) j;
@@ -48,5 +56,5 @@ public class SsrcGenerator {
 		byteWord[3] = (byte) (i & 0x00FF);
 		return byteWord;
 	}
-	
+
 }
