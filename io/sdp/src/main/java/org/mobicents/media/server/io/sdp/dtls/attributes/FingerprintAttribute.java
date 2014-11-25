b30b440f4d2bfe6c73c9eca426287af8602184cb
Henrique Rosa <henrique.rosa@telestax.com>
2014-11-25 20:33:03 +0000

    MEDIA-55 #comment Added parser for DTLS fingerprint

--- io/sdp/src/main/java/org/mobicents/media/server/io/sdp/dtls/attributes/FingerprintAttribute.java
+++ io/sdp/src/main/java/org/mobicents/media/server/io/sdp/dtls/attributes/FingerprintAttribute.java
@@ -3,17 +3,65 @@ package org.mobicents.media.server.io.sdp.dtls.attributes;
 import org.mobicents.media.server.io.sdp.fields.AttributeField;
 
 /**
+ * a=fingerprint:[hash-function][fingerprint]<br>
+ * 
+ * <p>
+ * Example:<br>
+ * a=fingerprint:sha-256 D1:2C:BE:AD:C4:F6:64:5C:25:16:11:9C:AF:E7:0F:73:79:36:
+ * 4E:9C:1E:15:54:39:0C:06:8B:ED:96:86:00:39
+ * </p>
+ * 
+ * <p>
+ * The fingerprint is the result of a hash function of the certificates used in
+ * the DTLS-SRTP negotiation. This line creates a binding between the signaling
+ * (which is supposed to be trusted) and the certificates used in DTLS, if the
+ * fingerprint doesn’t match, then the session should be rejected.
+ * </p>
  * 
  * 
  * @author Henrique Rosa (henrique.rosa@telestax.com)
- *
+ * 
+ * @see <a href="https://tools.ietf.org/html/rfc4572#section-5">RFC4572</a>
  */
 public class FingerprintAttribute extends AttributeField {
 
 	private static final String NAME = "fingerprint";
-	
+
+	private String hashFunction;
+	private String fingerprint;
+
 	public FingerprintAttribute() {
+		this(null, null);
+	}
+
+	public FingerprintAttribute(String hashFunction, String fingerprint) {
 		super(NAME);
+		this.hashFunction = hashFunction;
+		this.fingerprint = fingerprint;
+	}
+
+	public String getHashFunction() {
+		return hashFunction;
+	}
+
+	public void setHashFunction(String hashFunction) {
+		this.hashFunction = hashFunction;
+	}
+
+	public String getFingerprint() {
+		return fingerprint;
+	}
+
+	public void setFingerprint(String fingerprint) {
+		this.fingerprint = fingerprint;
+	}
+
+	@Override
+	public String toString() {
+		super.builder.setLength(0);
+		super.builder.append(BEGIN).append(NAME).append(ATTRIBUTE_SEPARATOR);
+		super.builder.append(this.hashFunction).append(" ").append(this.fingerprint);
+		return super.builder.toString();
 	}
 
 }
---
+++ io/sdp/src/test/java/org/mobicents/media/server/io/sdp/dtls/attributes/parser/FingerprintAttributeParserTest.java
@@ -0,0 +1,79 @@
+package org.mobicents.media.server.io.sdp.dtls.attributes.parser;
+
+import org.junit.Test;
+import org.mobicents.media.server.io.sdp.SdpException;
+import org.mobicents.media.server.io.sdp.dtls.attributes.FingerprintAttribute;
+
+import junit.framework.Assert;
+
+/**
+ * 
+ * @author Henrique Rosa (henrique.rosa@telestax.com)
+ * 
+ */
+public class FingerprintAttributeParserTest {
+
+	private final FingerprintAttributeParser parser = new FingerprintAttributeParser();
+
+	@Test
+	public void testCanParse() {
+		// given
+		String sdp1 = "a=fingerprint:sha-256 D1:2C:BE:AD:C4:F6:64:5C:25:16:11:9C:AF:E7:0F:73:79:36:4E:9C:1E:15:54:39:0C:06:8B:ED:96:86:00:39";
+		String sdp2 = "x=fingerprint:sha-256 D1:2C:BE:AD:C4:F6:64:5C:25:16:11:9C:AF:E7:0F:73:79:36:4E:9C:1E:15:54:39:0C:06:8B:ED:96:86:00:39";
+		String sdp3 = "a=finger:sha-256 D1:2C:BE:AD:C4:F6:64:5C:25:16:11:9C:AF:E7:0F:73:79:36:4E:9C:1E:15:54:39:0C:06:8B:ED:96:86:00:39";
+		String sdp4 = "a=fingerprint:D1:2C:BE:AD:C4:F6:64:5C:25:16:11:9C:AF:E7:0F:73:79:36:4E:9C:1E:15:54:39:0C:06:8B:ED:96:86:00:39";
+		String sdp5 = "a=fingerprint:sha-256 D1:2C:BE:AD:C4:F6:64:5C:25:16:11:9C:AF:E7:0F:73:79:36:4E:9C:1E:15:54:39:0C:06:8B:ED:96:86:00:39:";
+		String sdp6 = "a=fingerprint:sha-256 D1:2C:BE: AD:C4:F6:64:5C:25:16:11:9C:AF:E7:0F:73:79:36:4E:9C:1E:15:54:39:0C:06:8B:ED:96:86:00:39:";
+		String sdp7 = "a=fingerprint:sha-256";
+		
+		// when
+		boolean canParseSdp1 = parser.canParse(sdp1);
+		boolean canParseSdp2 = parser.canParse(sdp2);
+		boolean canParseSdp3 = parser.canParse(sdp3);
+		boolean canParseSdp4 = parser.canParse(sdp4);
+		boolean canParseSdp5 = parser.canParse(sdp5);
+		boolean canParseSdp6 = parser.canParse(sdp6);
+		boolean canParseSdp7 = parser.canParse(sdp7);
+		
+		// then
+		Assert.assertTrue(canParseSdp1);
+		Assert.assertFalse(canParseSdp2);
+		Assert.assertFalse(canParseSdp3);
+		Assert.assertFalse(canParseSdp4);
+		Assert.assertFalse(canParseSdp5);
+		Assert.assertFalse(canParseSdp6);
+		Assert.assertFalse(canParseSdp7);
+	}
+	
+	@Test
+	public void testParse() throws SdpException {
+		// given
+		String sdp1 = "a=fingerprint:sha-256 D1:2C:BE:AD:C4:F6:64:5C:25:16:11:9C:AF:E7:0F:73:79:36:4E:9C:1E:15:54:39:0C:06:8B:ED:96:86:00:39";
+
+		// when
+		FingerprintAttribute obj = parser.parse(sdp1);
+
+		// then
+		Assert.assertEquals("sha-256", obj.getHashFunction());
+		Assert.assertEquals("D1:2C:BE:AD:C4:F6:64:5C:25:16:11:9C:AF:E7:0F:73:79:36:4E:9C:1E:15:54:39:0C:06:8B:ED:96:86:00:39", obj.getFingerprint());
+	}
+
+	@Test(expected=SdpException.class)
+	public void testParseMissingHashFunction() throws SdpException {
+		// given
+		String sdp1 = "a=fingerprint:D1:2C:BE:AD:C4:F6:64:5C:25:16:11:9C:AF:E7:0F:73:79:36:4E:9C:1E:15:54:39:0C:06:8B:ED:96:86:00:39";
+		
+		// when
+		parser.parse(sdp1);
+	}
+
+	@Test(expected=SdpException.class)
+	public void testParseMissingFingerprint() throws SdpException {
+		// given
+		String sdp1 = "a=fingerprint:sha-256";
+		
+		// when
+		parser.parse(sdp1);
+	}
+	
+}
