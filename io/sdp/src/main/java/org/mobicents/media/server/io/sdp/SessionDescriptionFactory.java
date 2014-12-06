de9fe92b59767a986963e0d166a4c8e9523a1ad7
Henrique Rosa <henrique.rosa@telestax.com>
2014-12-06 02:25:19 +0000

    MEDIA-55 #comment added utility methods to query SDP and media attributes

---
+++ io/sdp/src/main/java/org/mobicents/media/server/io/sdp/SessionDescriptionFactory.java
@@ -0,0 +1,11 @@
+package org.mobicents.media.server.io.sdp;
+
+/**
+ * Factory that produces {@link SessionDescription}.
+ * 
+ * @author Henrique Rosa (henrique.rosa@telestax.com)
+ * 
+ */
+public class SessionDescriptionFactory {
+
+}
