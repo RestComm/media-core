5788e4d23a604e95e1f635f5d1b3ec4995617a72
hrosa <hdavidrosa@gmail.com>
2015-08-11 00:12:45 +0100

    #28 Working on a generic InbandComponent that will replace the AudioComponent. It will receive inband media bitstreams, either video or audio, and broadcast it over all registered media outputs.

--- component/src/main/java/org/mobicents/media/server/component/InbandComponent.java
+++ component/src/main/java/org/mobicents/media/server/component/InbandComponent.java
@@ -21,7 +21,13 @@
 
 package org.mobicents.media.server.component;
 
+import java.util.Iterator;
+
+import org.mobicents.media.server.component.audio.AudioOutput;
 import org.mobicents.media.server.concurrent.ConcurrentMap;
+import org.mobicents.media.server.spi.format.Format;
+import org.mobicents.media.server.spi.memory.Frame;
+import org.mobicents.media.server.spi.memory.Memory;
 
 /**
  * @author Henrique Rosa (henrique.rosa@telestax.com)
@@ -29,12 +35,90 @@ import org.mobicents.media.server.concurrent.ConcurrentMap;
  */
 public abstract class InbandComponent {
 
+    private final int componentId;
     private final ConcurrentMap<MediaInput> inputs;
     private final ConcurrentMap<MediaOutput> outputs;
+    private boolean readable;
+    private boolean writable;
 
-    public InbandComponent() {
+    public InbandComponent(int componentId) {
+        this.componentId = componentId;
         this.inputs = new ConcurrentMap<MediaInput>();
         this.outputs = new ConcurrentMap<MediaOutput>();
+        this.readable = false;
+        this.writable = false;
+    }
+
+    public int getComponentId() {
+        return componentId;
+    }
+
+    public boolean isReadable() {
+        return readable;
+    }
+
+    public void setReadable(boolean readable) {
+        this.readable = readable;
+    }
+
+    public boolean isWritable() {
+        return writable;
+    }
+
+    public void setWritable(boolean writable) {
+        this.writable = writable;
+    }
+
+    public void addInput(MediaInput input) {
+        this.inputs.put(input.getInputId(), input);
+    }
+
+    public void releaseInput(MediaInput input) {
+        this.inputs.remove(input.getInputId());
+    }
+
+    public void addOutput(MediaOutput output) {
+        this.outputs.put(output.getOutputId(), output);
+    }
+
+    public void releaseOutput(MediaOutput output) {
+        this.outputs.remove(output.getOutputId());
+    }
+
+    public int[] retrieveData() {
+
+    }
+
+    public void submitData(int[] data, Format format) {
+        if (this.writable) {
+            // Allocate a new frame
+            Frame frame = Memory.allocate(data.length);
+            frame.setOffset(0);
+            frame.setLength(PACKET_SIZE);
+            frame.setDuration(PERIOD);
+            frame.setFormat(LINEAR_FORMAT);
+
+            int index = 0;
+            int count = 0;
+            byte[] payload = frame.getData();
+
+            while (count < data.length) {
+                payload[index++] = (byte) (data[count]);
+                payload[index++] = (byte) (data[count++] >> 8);
+            }
+
+            // Send frame to all registered outputs
+            Iterator<MediaOutput> activeOutputs = this.outputs.valuesIterator();
+            while (activeOutputs.hasNext()) {
+                MediaOutput output = activeOutputs.next();
+                if (activeOutputs.hasNext()) {
+                    output.offer(frame.clone());
+                } else {
+                    output.offer(frame);
+                }
+                output.wakeup();
+            }
+        }
     }
 
 }
