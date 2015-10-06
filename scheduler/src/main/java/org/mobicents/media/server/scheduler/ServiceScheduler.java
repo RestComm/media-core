4b05f4a2ea9e4f47df01e01fcc6319db838243cc
hrosa <hdavidrosa@gmail.com>
2015-10-06 13:05:30 +0100

    #34 Created Scheduler interface which allows users to submit Runnables to be executed according to time intervals.
    Implemented ServiceScheduler which relies on ScheduledExecutorService to manage the thread pool as well as the scheduled tasks.

---
+++ scheduler/src/main/java/org/mobicents/media/server/scheduler/ServiceScheduler.java
@@ -0,0 +1,121 @@
+/*
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2015, Telestax Inc and individual contributors
+ * by the @authors tag. 
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.mobicents.media.server.scheduler;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Scheduler implementation that relies on a {@link ScheduledExecutorService} to manage the thread pool as well as the scheduled
+ * tasks.
+ * 
+ * @author Henrique Rosa (henrique.rosa@telestax.com)
+ *
+ */
+public class ServiceScheduler implements Scheduler {
+
+    private static final int POOL_SIZE = Runtime.getRuntime().availableProcessors() * 2;
+
+    private volatile boolean started;
+    private final Clock wallClock;
+    private final ScheduledExecutorService executor;
+    private final ThreadFactory threadFactory = new ThreadFactory() {
+
+        private AtomicInteger index = new AtomicInteger(0);
+
+        @Override
+        public Thread newThread(Runnable r) {
+            return new Thread(r, "service-scheduler-" + index.incrementAndGet());
+        }
+    };
+
+    public ServiceScheduler(Clock clock) {
+        this.started = false;
+        this.wallClock = clock;
+        this.executor = Executors.newScheduledThreadPool(POOL_SIZE, threadFactory);
+    }
+
+    @Override
+    public Clock getWallClock() {
+        return this.wallClock;
+    }
+
+    @Override
+    public Future<?> submit(Runnable task) throws RejectedExecutionException {
+        if (!this.started) {
+            throw new RejectedExecutionException("Scheduler is not running.");
+        }
+        return this.executor.submit(task);
+    }
+
+    @Override
+    public ScheduledFuture<?> schedule(Runnable task, long delay, TimeUnit unit) throws RejectedExecutionException {
+        if (!this.started) {
+            throw new RejectedExecutionException("Scheduler is not running.");
+        }
+        return this.executor.schedule(task, delay, unit);
+    }
+
+    @Override
+    public ScheduledFuture<?> scheduleWithFixedDelay(Runnable task, long initialDelay, long period, TimeUnit unit)
+            throws IllegalArgumentException, RejectedExecutionException {
+        if (!this.started) {
+            throw new RejectedExecutionException("Scheduler is not running.");
+        }
+        return this.executor.scheduleWithFixedDelay(task, initialDelay, period, unit);
+    }
+
+    @Override
+    public void start() {
+        if (!this.started) {
+            this.started = true;
+        }
+    }
+
+    @Override
+    public void stop() {
+        if (this.started) {
+            this.started = false;
+            this.executor.shutdown();
+        }
+    }
+
+    @Override
+    public void stopNow() {
+        if (this.started) {
+            this.started = false;
+            this.executor.shutdownNow();
+        }
+    }
+
+    @Override
+    public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+        return this.executor.awaitTermination(timeout, unit);
+    }
+
+}
