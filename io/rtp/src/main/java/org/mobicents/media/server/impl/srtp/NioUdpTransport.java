1ad827fffe7dcf83388799f71fa2896b20204f8b
hrosa <hdavidrosa@gmail.com>
2015-12-18 16:57:21 +0000

    Increased DTLS handshake timeout to 10seconds.

--- io/rtp/src/main/java/org/mobicents/media/server/impl/srtp/NioUdpTransport.java
+++ io/rtp/src/main/java/org/mobicents/media/server/impl/srtp/NioUdpTransport.java
@@ -44,7 +44,7 @@ public class NioUdpTransport implements DatagramTransport {
 	private final static int MIN_IP_OVERHEAD = 20;
 	private final static int MAX_IP_OVERHEAD = MIN_IP_OVERHEAD + 64;
 	private final static int UDP_OVERHEAD = 8;
-	public final static int MAX_DELAY = 4000;
+	public final static int MAX_DELAY = 10000;
 
 	private final DatagramChannel channel;
 	private int mtu;
