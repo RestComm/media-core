5c8fc6125d6debdb87ed90c4f2c882901b273eda
hrosa <hdavidrosa@gmail.com>
2016-05-31 17:12:25 +0100

    #162 Code formatting and protecting logger statements.

--- controls/mgcp/src/main/java/org/mobicents/media/server/mgcp/pkg/au/Play.java
+++ controls/mgcp/src/main/java/org/mobicents/media/server/mgcp/pkg/au/Play.java
@@ -31,7 +31,6 @@ import org.mobicents.media.ComponentType;
 import org.mobicents.media.server.mgcp.controller.signal.Event;
 import org.mobicents.media.server.mgcp.controller.signal.NotifyImmediately;
 import org.mobicents.media.server.mgcp.controller.signal.Signal;
-import org.mobicents.media.server.spi.Endpoint;
 import org.mobicents.media.server.spi.MediaType;
 import org.mobicents.media.server.spi.ResourceUnavailableException;
 import org.mobicents.media.server.spi.listener.TooManyListenersException;
@@ -44,103 +43,125 @@ import org.mobicents.media.server.utils.Text;
  * Implements play announcement signal.
  * 
  * @author yulian oifa
+ * @author Henrique Rosa (henrique.rosa@telestax.com)
  */
 public class Play extends Signal implements PlayerListener {
-	
-    private Event oc = new Event(new Text("oc"));
-    private Event of = new Event(new Text("of"));
-    
+
+    private final static Logger logger = Logger.getLogger(Play.class);
+
+    // Response Messages
+    private static final Text MSG_NO_PLAYER = new Text("Endpoint has no player");
+    private static final Text MSG_RC_301 = new Text("rc=301");
+    private static final Text MSG_RC_312 = new Text("rc=312");
+    private static final Text MSG_RC_100 = new Text("rc=100");
+
+    // Media Components
     private Player player;
     private volatile Options options;
-    
+
+    // Play Properties
+    private Iterator<Text> segments;
     private int repeatCount;
     private int segCount;
-    
-    
     private long delay;
     private String uri;
-    
-    private Iterator<Text> segments;
-    private final static Logger logger = Logger.getLogger(Play.class);
-    
-    private Semaphore terminateSemaphore=new Semaphore(1);
-    
+
+    // MGCP Properties
+    private final Event oc;
+    private final Event of;
+
+    // Concurrency Properties
+    private final Semaphore terminateSemaphore;
+
     public Play(String name) {
         super(name);
-        oc.add(new NotifyImmediately("N"));
-        of.add(new NotifyImmediately("N"));                
+
+        // MGCP Properties
+        this.oc = new Event(new Text("oc"));
+        this.oc.add(new NotifyImmediately("N"));
+        this.of = new Event(new Text("of"));
+        this.of.add(new NotifyImmediately("N"));
+
+        // Concurrency Properties
+        this.terminateSemaphore = new Semaphore(1);
     }
-    
+
     @Override
     public void execute() {
-    	//get access to player
+        // get access to player
         player = this.getPlayer();
 
-        //check result
+        // check result
         if (player == null) {
-            of.fire(this, new Text("Endpoint has no player"));
+            of.fire(this, MSG_NO_PLAYER);
             complete();
             return;
         }
-        
-        //register announcement handler
+
+        // register announcement handler
         try {
             player.addListener(this);
         } catch (TooManyListenersException e) {
             logger.error("OPERATION FAILURE", e);
-        } 
-        
-        //get options of the request
-        options = Options.allocate(getTrigger().getParams());        
-                
-        //set initial delay
+        }
+
+        // get options of the request
+        options = Options.allocate(getTrigger().getParams());
+
+        // set initial delay
         delay = 0;
-        
-        //get announcement segments
+
+        // get announcement segments
         segments = options.getSegments().iterator();
         repeatCount = options.getRepeatCount();
-        segCount=0;
-        
+        segCount = 0;
+
         uri = segments.next().toString();
 
         // start announcement
-        startAnnouncementPhase();      
+        startAnnouncementPhase();
     }
-    
+
     private void startAnnouncementPhase() {
-        logger.info(String.format("(%s) Start announcement (segment=%d)", getEndpoint().getLocalName(), segCount));
-        
+        if (logger.isInfoEnabled()) {
+            logger.info(String.format("(%s) Start announcement (segment=%d)", getEndpoint().getLocalName(), segCount));
+        }
+
         try {
             player.setURL(uri);
         } catch (MalformedURLException e) {
-        	logger.info("Received URL in invalid format , firing of");
-            of.fire(this, new Text("rc=301"));
-            complete();            
+            if (logger.isInfoEnabled()) {
+                logger.info("Received URL in invalid format , firing of");
+            }
+            of.fire(this, MSG_RC_301);
+            complete();
             return;
         } catch (ResourceUnavailableException e) {
-        	logger.info("Received URL can not be found , firing of");
-            of.fire(this, new Text("rc=312"));
+            if (logger.isInfoEnabled()) {
+                logger.info("Received URL can not be found , firing of");
+            }
+            of.fire(this, MSG_RC_312);
             complete();
             return;
         }
-        
-        //set max duration if present
+
+        // set max duration if present
         if (options.getDuration() != -1) {
             player.setDuration(options.getDuration());
         }
 
-        //set initial offset
+        // set initial offset
         if (options.getOffset() > 0) {
             player.setMediaTime(options.getOffset());
         }
 
-        //initial delay
+        // initial delay
         player.setInitialDelay(delay);
 
-        //starting
+        // starting
         player.activate();
-    }    
-    
+    }
+
     @Override
     public boolean doAccept(Text event) {
         if (!oc.isActive() && oc.matches(event)) {
@@ -150,104 +171,97 @@ public class Play extends Signal implements PlayerListener {
         if (!of.isActive() && of.matches(event)) {
             return true;
         }
-        
+
         return false;
     }
 
     @Override
     public void cancel() {
-    	terminate();                
+        terminate();
     }
 
     private Player getPlayer() {
-    	Endpoint endpoint = getEndpoint();
         return (Player) getEndpoint().getResource(MediaType.AUDIO, ComponentType.PLAYER);
     }
-        
+
     @Override
     public void reset() {
         super.reset();
         terminate();
-        
+
         oc.reset();
         of.reset();
-        
     }
-    
-    private void terminate() 
-    {    	
-    	try
-    	{
-    		terminateSemaphore.acquire();
-    	}
-    	catch(InterruptedException e)
-    	{
-    		
-    	}
-    	
-    	if (player != null) 
-    	{    		
-    		player.removeListener(this);
-    		player.deactivate();
-    		player=null;
-        }    	    	
-    	
-    	if(options!=null)
-    	{
-    		Options.recycle(options);    	
-    		options=null;
-    	}
-    	
-    	terminateSemaphore.release();    	    	
+
+    private void terminate() {
+        try {
+            terminateSemaphore.acquire();
+        } catch (InterruptedException e) {
+
+        }
+
+        if (player != null) {
+            player.removeListener(this);
+            player.deactivate();
+            player = null;
+        }
+
+        if (options != null) {
+            Options.recycle(options);
+            options = null;
+        }
+
+        terminateSemaphore.release();
     }
-    
+
     private void repeat(long delay) {
         this.delay = delay;
         startAnnouncementPhase();
     }
-    
+
     private void next(long delay) {
         uri = segments.next().toString();
         segCount++;
-        
+
         this.delay = delay;
         startAnnouncementPhase();
     }
 
     public void process(PlayerEvent event) {
         switch (event.getID()) {
-            case PlayerEvent.STOP :
-                logger.info(String.format("(%s) Announcement (segment=%d) has completed", getEndpoint().getLocalName(), segCount));
-                if(repeatCount==-1)
-                {
-                	repeat(options.getInterval());
-            		return;            		                	
+            case PlayerEvent.STOP:
+                if (logger.isInfoEnabled()) {
+                    logger.info(String.format("(%s) Announcement (segment=%d) has completed", getEndpoint().getLocalName(),
+                            segCount));
                 }
-                else
-                {
-                	repeatCount--;
-                    
-                	if (repeatCount > 0) {
-                		repeat(options.getInterval());
-                		return;
-                	}
+
+                if (repeatCount == -1) {
+                    repeat(options.getInterval());
+                    return;
+                } else {
+                    repeatCount--;
+
+                    if (repeatCount > 0) {
+                        repeat(options.getInterval());
+                        return;
+                    }
                 }
-                
+
                 if (segments.hasNext()) {
-                	repeatCount = options.getRepeatCount();
+                    repeatCount = options.getRepeatCount();
                     next(options.getInterval());
                     return;
                 }
-                
+
                 terminate();
-                oc.fire(this, new Text("rc=100"));
+                oc.fire(this, MSG_RC_100);
                 this.complete();
-                
+
                 break;
-            case PlayerEvent.FAILED :
-            	terminate();
+            case PlayerEvent.FAILED:
+                terminate();
                 oc.fire(this, null);
                 this.complete();
         }
-    }        
+    }
 }
