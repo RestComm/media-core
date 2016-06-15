6added70605e9d0a9fe4b5e82d39f3e6e432426b
hrosa <hdavidrosa@gmail.com>
2016-06-15 11:58:27 +0100

    #136 Writing unit tests for MDCX

--- controls/mgcp2/src/test/java/org/mobicents/media/control/mgcp/command/ModifyConnectionCommandTest.java
+++ controls/mgcp2/src/test/java/org/mobicents/media/control/mgcp/command/ModifyConnectionCommandTest.java
@@ -36,6 +36,7 @@ import org.mobicents.media.control.mgcp.connection.MgcpConnectionProvider;
 import org.mobicents.media.control.mgcp.connection.MgcpRemoteConnection;
 import org.mobicents.media.control.mgcp.endpoint.MgcpEndpoint;
 import org.mobicents.media.control.mgcp.endpoint.MgcpEndpointManager;
+import org.mobicents.media.control.mgcp.exception.MgcpConnectionException;
 import org.mobicents.media.control.mgcp.exception.MgcpException;
 import org.mobicents.media.control.mgcp.listener.MgcpCommandListener;
 import org.mobicents.media.control.mgcp.message.MgcpMessageParser;
@@ -109,7 +110,7 @@ public class ModifyConnectionCommandTest {
         builderSdp.append("m=audio 7070 RTP/AVP 8 0 101").append(System.lineSeparator());
         builderSdp.append("a=rtpmap:101 telephone-event/8000");
         builder.append(builderSdp.toString());
-        
+
         final MgcpMessageParser parser = new MgcpMessageParser();
         final MgcpRequest request = parser.parseRequest(builder.toString());
         final MgcpEndpointManager endpointManager = mock(MgcpEndpointManager.class);
@@ -118,12 +119,12 @@ public class ModifyConnectionCommandTest {
         final MgcpRemoteConnection connection = mock(MgcpRemoteConnection.class);
         final MgcpCommandListener listener = mock(MgcpCommandListener.class);
         final ModifyConnectionCommand mdcx = new ModifyConnectionCommand(endpointManager, connectionProvider);
-        
+
         // when
         when(endpointManager.getEndpoint("mobicents/bridge/1")).thenReturn(bridgeEndpoint);
         when(bridgeEndpoint.getConnection(1, 1)).thenReturn(connection);
         doAnswer(new Answer<Object>() {
-            
+
             @Override
             public Object answer(InvocationOnMock invocation) throws Throwable {
                 MgcpResponse response = invocation.getArgumentAt(0, MgcpResponse.class);
@@ -131,13 +132,295 @@ public class ModifyConnectionCommandTest {
                 assertEquals(MgcpResponseCode.TRANSACTION_WAS_EXECUTED.code(), response.getCode());
                 return null;
             }
-            
+
         }).when(listener).onCommandExecuted(any(MgcpResponse.class));
         mdcx.execute(request, listener);
-        
+
         // then
         verify(connection, times(1)).setMode(ConnectionMode.SEND_RECV);
         verify(connection, times(1)).open(builderSdp.toString());
+    }
+
+    @Test
+    public void testValidateRequestWithEndpointNameContainingWildCardAll() throws MgcpException {
+        // given
+        final StringBuilder builder = new StringBuilder();
+        builder.append("MDCX 147483653 mobicents/bridge/*@127.0.0.1:2427 MGCP 1.0").append(System.lineSeparator());
+        builder.append("C:1").append(System.lineSeparator());
+        builder.append("I:1").append(System.lineSeparator());
+        builder.append("M:sendrecv").append(System.lineSeparator());
+        builder.append(System.lineSeparator());
+
+        final MgcpMessageParser parser = new MgcpMessageParser();
+        final MgcpRequest request = parser.parseRequest(builder.toString());
+        final MgcpEndpointManager endpointManager = mock(MgcpEndpointManager.class);
+        final MgcpConnectionProvider connectionProvider = mock(MgcpConnectionProvider.class);
+        final MgcpCommandListener listener = mock(MgcpCommandListener.class);
+        final ModifyConnectionCommand mdcx = new ModifyConnectionCommand(endpointManager, connectionProvider);
+
+        // when
+        doAnswer(new Answer<Object>() {
+
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                // then
+                MgcpResponse response = invocation.getArgumentAt(0, MgcpResponse.class);
+                assertNotNull(response);
+                assertEquals(MgcpResponseCode.WILDCARD_TOO_COMPLICATED.code(), response.getCode());
+                return null;
+            }
+
+        }).when(listener).onCommandExecuted(any(MgcpResponse.class));
+        mdcx.execute(request, listener);
+    }
+
+    @Test
+    public void testValidateRequestWithEndpointNameContainingWildCardAny() throws MgcpException {
+        // given
+        final StringBuilder builder = new StringBuilder();
+        builder.append("MDCX 147483653 mobicents/bridge/$@127.0.0.1:2427 MGCP 1.0").append(System.lineSeparator());
+        builder.append("C:1").append(System.lineSeparator());
+        builder.append("I:1").append(System.lineSeparator());
+        builder.append("M:sendrecv").append(System.lineSeparator());
+        builder.append(System.lineSeparator());
+
+        final MgcpMessageParser parser = new MgcpMessageParser();
+        final MgcpRequest request = parser.parseRequest(builder.toString());
+        final MgcpEndpointManager endpointManager = mock(MgcpEndpointManager.class);
+        final MgcpConnectionProvider connectionProvider = mock(MgcpConnectionProvider.class);
+        final MgcpCommandListener listener = mock(MgcpCommandListener.class);
+        final ModifyConnectionCommand mdcx = new ModifyConnectionCommand(endpointManager, connectionProvider);
+
+        // when
+        doAnswer(new Answer<Object>() {
+
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                // then
+                MgcpResponse response = invocation.getArgumentAt(0, MgcpResponse.class);
+                assertNotNull(response);
+                assertEquals(MgcpResponseCode.WILDCARD_TOO_COMPLICATED.code(), response.getCode());
+                return null;
+            }
+
+        }).when(listener).onCommandExecuted(any(MgcpResponse.class));
+        mdcx.execute(request, listener);
+    }
+
+    @Test
+    public void testValidateRequestMissingCallId() throws MgcpException {
+        // given
+        final StringBuilder builder = new StringBuilder();
+        builder.append("MDCX 147483653 mobicents/bridge/1@127.0.0.1:2427 MGCP 1.0").append(System.lineSeparator());
+        builder.append("I:1").append(System.lineSeparator());
+        builder.append("M:sendrecv").append(System.lineSeparator());
+        builder.append(System.lineSeparator());
+
+        final MgcpMessageParser parser = new MgcpMessageParser();
+        final MgcpRequest request = parser.parseRequest(builder.toString());
+        final MgcpEndpointManager endpointManager = mock(MgcpEndpointManager.class);
+        final MgcpConnectionProvider connectionProvider = mock(MgcpConnectionProvider.class);
+        final MgcpCommandListener listener = mock(MgcpCommandListener.class);
+        final ModifyConnectionCommand mdcx = new ModifyConnectionCommand(endpointManager, connectionProvider);
+
+        // when
+        doAnswer(new Answer<Object>() {
+
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                // then
+                MgcpResponse response = invocation.getArgumentAt(0, MgcpResponse.class);
+                assertNotNull(response);
+                assertEquals(MgcpResponseCode.INCORRECT_CALL_ID.code(), response.getCode());
+                return null;
+            }
+
+        }).when(listener).onCommandExecuted(any(MgcpResponse.class));
+        mdcx.execute(request, listener);
+    }
+
+    @Test
+    public void testValidateRequestMissingConnectionId() throws MgcpException {
+        // given
+        final StringBuilder builder = new StringBuilder();
+        builder.append("MDCX 147483653 mobicents/bridge/1@127.0.0.1:2427 MGCP 1.0").append(System.lineSeparator());
+        builder.append("C:1").append(System.lineSeparator());
+        builder.append("M:sendrecv").append(System.lineSeparator());
+        builder.append(System.lineSeparator());
+
+        final MgcpMessageParser parser = new MgcpMessageParser();
+        final MgcpRequest request = parser.parseRequest(builder.toString());
+        final MgcpEndpointManager endpointManager = mock(MgcpEndpointManager.class);
+        final MgcpConnectionProvider connectionProvider = mock(MgcpConnectionProvider.class);
+        final MgcpCommandListener listener = mock(MgcpCommandListener.class);
+        final ModifyConnectionCommand mdcx = new ModifyConnectionCommand(endpointManager, connectionProvider);
+
+        // when
+        doAnswer(new Answer<Object>() {
+
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                // then
+                MgcpResponse response = invocation.getArgumentAt(0, MgcpResponse.class);
+                assertNotNull(response);
+                assertEquals(MgcpResponseCode.INCORRECT_CONNECTION_ID.code(), response.getCode());
+                return null;
+            }
+
+        }).when(listener).onCommandExecuted(any(MgcpResponse.class));
+        mdcx.execute(request, listener);
+    }
+
+    @Test
+    public void testValidateRequestWithInvalidMode() throws MgcpException {
+        // given
+        final StringBuilder builder = new StringBuilder();
+        builder.append("MDCX 147483653 mobicents/bridge/1@127.0.0.1:2427 MGCP 1.0").append(System.lineSeparator());
+        builder.append("C:1").append(System.lineSeparator());
+        builder.append("I:1").append(System.lineSeparator());
+        builder.append("M:xywz").append(System.lineSeparator());
+        builder.append(System.lineSeparator());
+
+        final MgcpMessageParser parser = new MgcpMessageParser();
+        final MgcpRequest request = parser.parseRequest(builder.toString());
+        final MgcpEndpointManager endpointManager = mock(MgcpEndpointManager.class);
+        final MgcpConnectionProvider connectionProvider = mock(MgcpConnectionProvider.class);
+        final MgcpCommandListener listener = mock(MgcpCommandListener.class);
+        final ModifyConnectionCommand mdcx = new ModifyConnectionCommand(endpointManager, connectionProvider);
+
+        // when
+        doAnswer(new Answer<Object>() {
+
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                // then
+                MgcpResponse response = invocation.getArgumentAt(0, MgcpResponse.class);
+                assertNotNull(response);
+                assertEquals(MgcpResponseCode.INVALID_OR_UNSUPPORTED_MODE.code(), response.getCode());
+                return null;
+            }
+
+        }).when(listener).onCommandExecuted(any(MgcpResponse.class));
+        mdcx.execute(request, listener);
+    }
+
+    @Test
+    public void testUnknownEndpoint() throws MgcpException {
+        // given
+        final StringBuilder builder = new StringBuilder();
+        builder.append("MDCX 147483653 mobicents/bridge/1@127.0.0.1:2427 MGCP 1.0").append(System.lineSeparator());
+        builder.append("C:1").append(System.lineSeparator());
+        builder.append("I:1").append(System.lineSeparator());
+        builder.append("M:sendonly").append(System.lineSeparator());
+
+        final MgcpMessageParser parser = new MgcpMessageParser();
+        final MgcpRequest request = parser.parseRequest(builder.toString());
+        final MgcpEndpointManager endpointManager = mock(MgcpEndpointManager.class);
+        final MgcpConnectionProvider connectionProvider = mock(MgcpConnectionProvider.class);
+        final MgcpEndpoint bridgeEndpoint = mock(MgcpEndpoint.class);
+        final MgcpRemoteConnection connection = mock(MgcpRemoteConnection.class);
+        final MgcpCommandListener listener = mock(MgcpCommandListener.class);
+        final ModifyConnectionCommand mdcx = new ModifyConnectionCommand(endpointManager, connectionProvider);
+
+        // when
+        when(endpointManager.getEndpoint("mobicents/bridge/1")).thenReturn(null);
+        when(bridgeEndpoint.getConnection(1, 1)).thenReturn(connection);
+        doAnswer(new Answer<Object>() {
+
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                // then
+                MgcpResponse response = invocation.getArgumentAt(0, MgcpResponse.class);
+                assertNotNull(response);
+                assertEquals(MgcpResponseCode.ENDPOINT_UNKNOWN.code(), response.getCode());
+                return null;
+            }
+
+        }).when(listener).onCommandExecuted(any(MgcpResponse.class));
+        mdcx.execute(request, listener);
+    }
+
+    @Test
+    public void testUnknownConnection() throws MgcpException {
+        // given
+        final StringBuilder builder = new StringBuilder();
+        builder.append("MDCX 147483653 mobicents/bridge/1@127.0.0.1:2427 MGCP 1.0").append(System.lineSeparator());
+        builder.append("C:1").append(System.lineSeparator());
+        builder.append("I:1").append(System.lineSeparator());
+        builder.append("M:sendonly").append(System.lineSeparator());
+
+        final MgcpMessageParser parser = new MgcpMessageParser();
+        final MgcpRequest request = parser.parseRequest(builder.toString());
+        final MgcpEndpointManager endpointManager = mock(MgcpEndpointManager.class);
+        final MgcpConnectionProvider connectionProvider = mock(MgcpConnectionProvider.class);
+        final MgcpEndpoint bridgeEndpoint = mock(MgcpEndpoint.class);
+        final MgcpCommandListener listener = mock(MgcpCommandListener.class);
+        final ModifyConnectionCommand mdcx = new ModifyConnectionCommand(endpointManager, connectionProvider);
+
+        // when
+        when(endpointManager.getEndpoint("mobicents/bridge/1")).thenReturn(bridgeEndpoint);
+        when(bridgeEndpoint.getConnection(1, 1)).thenReturn(null);
+        doAnswer(new Answer<Object>() {
+
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                // then
+                MgcpResponse response = invocation.getArgumentAt(0, MgcpResponse.class);
+                assertNotNull(response);
+                assertEquals(MgcpResponseCode.INCORRECT_CONNECTION_ID.code(), response.getCode());
+                return null;
+            }
+
+        }).when(listener).onCommandExecuted(any(MgcpResponse.class));
+        mdcx.execute(request, listener);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testInvalidRemoteDescription() throws MgcpException {
+        // given
+        final StringBuilder builder = new StringBuilder();
+        builder.append("MDCX 147483653 mobicents/bridge/1@127.0.0.1:2427 MGCP 1.0").append(System.lineSeparator());
+        builder.append("C:1").append(System.lineSeparator());
+        builder.append("I:1").append(System.lineSeparator());
+        builder.append("M:sendonly").append(System.lineSeparator());
+        builder.append(System.lineSeparator());
+        final StringBuilder builderSdp = new StringBuilder();
+        builderSdp.append("v=0").append(System.lineSeparator());
+        builderSdp.append("o=hrosa 3616 1899 IN IP4 127.0.0.1").append(System.lineSeparator());
+        builderSdp.append("s=Talk").append(System.lineSeparator());
+        builderSdp.append("c=IN IP4 127.0.0.1").append(System.lineSeparator());
+        builderSdp.append("t=0 0").append(System.lineSeparator());
+        builderSdp.append("m=audio 7070 RTP/AVP 8 0 101").append(System.lineSeparator());
+        builderSdp.append("a=rtpmap:101 telephone-event/8000");
+        builder.append(builderSdp.toString());
+
+        final MgcpMessageParser parser = new MgcpMessageParser();
+        final MgcpRequest request = parser.parseRequest(builder.toString());
+        final MgcpEndpointManager endpointManager = mock(MgcpEndpointManager.class);
+        final MgcpConnectionProvider connectionProvider = mock(MgcpConnectionProvider.class);
+        final MgcpEndpoint bridgeEndpoint = mock(MgcpEndpoint.class);
+        final MgcpRemoteConnection connection = mock(MgcpRemoteConnection.class);
+        final MgcpCommandListener listener = mock(MgcpCommandListener.class);
+        final ModifyConnectionCommand mdcx = new ModifyConnectionCommand(endpointManager, connectionProvider);
+
+        // when
+        when(endpointManager.getEndpoint("mobicents/bridge/1")).thenReturn(bridgeEndpoint);
+        when(bridgeEndpoint.getConnection(1, 1)).thenReturn(connection);
+        when(connection.open(builderSdp.toString())).thenThrow(MgcpConnectionException.class);
+        doAnswer(new Answer<Object>() {
+
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                // then
+                MgcpResponse response = invocation.getArgumentAt(0, MgcpResponse.class);
+                assertNotNull(response);
+                assertEquals(MgcpResponseCode.UNSUPPORTED_SDP.code(), response.getCode());
+                return null;
+            }
+
+        }).when(listener).onCommandExecuted(any(MgcpResponse.class));
+        mdcx.execute(request, listener);
     }
 
 }
