681920fb74390bf2f72b5ec5e580e0f4d7e5901c
hrosa <hdavidrosa@gmail.com>
2016-08-18 01:19:22 +0100

    Speeded up CnameGenerator and added concurrent test case.

--- io/rtp/src/test/java/org/mobicents/media/server/impl/rtp/CnameGeneratorTest.java
+++ io/rtp/src/test/java/org/mobicents/media/server/impl/rtp/CnameGeneratorTest.java
@@ -20,13 +20,23 @@
 
 package org.mobicents.media.server.impl.rtp;
 
-import java.util.ArrayList;
-import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.apache.log4j.Logger;
 import org.bouncycastle.util.encoders.Base64;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * Tests validity of generated CNAME to be used in RTP sessions.
@@ -36,27 +46,110 @@ import static org.junit.Assert.*;
  */
 public class CnameGeneratorTest {
 
-	@Test
-	public void testCnameGenerator() {
-		// given
-		int minCnameSize = 98;
-		int capacity = 10000;
-		List<String> cnameList = new ArrayList<String>(capacity);
+    private final static Logger log = Logger.getLogger(CnameGeneratorTest.class);
 
-		for (int i = 0; i < capacity; i++) {
-			// when
-			String cname = CnameGenerator.generateCname();
+    private final ExecutorService executor = Executors.newFixedThreadPool(3);
 
-			// then
-			// test minimum size
-			assertEquals((minCnameSize / 8), Base64.decode(cname).length);
-			// test uniqueness
-			assertFalse(cnameList.contains(cname));
-			cnameList.add(cname);
-		}
+    public void after() {
+        executor.shutdown();
+    }
 
-		// cleanup
-		cnameList.clear();
-	}
+    @Test(timeout = 10)
+    public void testCnameGeneratorSpeed() {
+        CnameGenerator.generateCname();
+    }
+
+    @Test
+    public void testCnameGenerator() {
+        // given
+        int minCnameSize = 98;
+        int capacity = 10000;
+        List<String> cnameList = new ArrayList<String>(capacity);
+
+        for (int i = 0; i < capacity; i++) {
+            // when
+            long start = System.currentTimeMillis();
+            String cname = CnameGenerator.generateCname();
+            long time = System.currentTimeMillis() - start;
+            System.out.println("Took " + time + " ms");
+
+            // then
+            // test minimum size
+            assertEquals((minCnameSize / 8), Base64.decode(cname).length);
+            // test uniqueness
+            assertFalse(cnameList.contains(cname));
+            cnameList.add(cname);
+        }
+
+        // cleanup
+        cnameList.clear();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testConcurrentCnameGenerator() throws InterruptedException, ExecutionException {
+        // given
+        int threads = Runtime.getRuntime().availableProcessors() * 2;
+        Future<Set<String>>[] futures = new Future[threads];
+
+        // when
+        for (int i = 0; i < threads; i++) {
+            Callable<Set<String>> worker = new GeneratorWorker("worker-" + i);
+            Future<Set<String>> future = executor.submit(worker);
+            futures[i] = future;
+        }
+        Thread.sleep(5000L);
+
+        // then
+        for (int i = 0; i < threads; i++) {
+            Future<Set<String>> futureA = futures[i];
+            for (int j = i + 1; j < futures.length; j++) {
+                Future<Set<String>> futureB = futures[j];
+
+                HashSet<String> intersection = new HashSet<String>(futureA.get());
+                intersection.retainAll(futureB.get());
+                assertTrue(intersection.isEmpty());
+            }
+        }
+    }
+
+    private class GeneratorWorker implements Callable<Set<String>> {
+
+        private final String name;
+
+        public GeneratorWorker(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public Set<String> call() throws Exception {
+            // given
+            int minCnameSize = 98;
+            int capacity = 10000;
+            Set<String> cnameList = new HashSet<String>(capacity);
+
+            for (int i = 0; i < capacity; i++) {
+                // when
+                long start = System.currentTimeMillis();
+                String cname = CnameGenerator.generateCname();
+                long time = System.currentTimeMillis() - start;
+
+                if (log.isInfoEnabled()) {
+                    log.info(this.name + " took " + time + "ms to generate the cname " + cname);
+                }
+
+                // then
+                // test minimum size
+                assertEquals((minCnameSize / 8), Base64.decode(cname).length);
+                // test uniqueness
+                assertFalse(cnameList.contains(cname));
+                cnameList.add(cname);
+            }
+
+            // cleanup
+            return cnameList;
+        }
+
+    }
 
 }
