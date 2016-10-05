d0c565a792fcff05e1a2ef7e769d907729b7d7cd
Gennadiy Dubina <gdubina@dataart.com>
2016-10-05 14:51:20 +0300

    remove uri member

--- resources/mediaplayer/src/main/java/org/mobicents/media/server/impl/resource/mediaplayer/audio/CachedRemoteStreamProvider.java
+++ resources/mediaplayer/src/main/java/org/mobicents/media/server/impl/resource/mediaplayer/audio/CachedRemoteStreamProvider.java
@@ -25,43 +25,37 @@ public class CachedRemoteStreamProvider implements RemoteStreamProvider {
     public CachedRemoteStreamProvider(int size) {
         cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
                 .withCache("preConfigured",
-                        CacheConfigurationBuilder.newCacheConfigurationBuilder(URL.class, AudioStreamCache.class,
+                        CacheConfigurationBuilder.newCacheConfigurationBuilder(URL.class, ByteStreamCache.class,
                                 ResourcePoolsBuilder.newResourcePoolsBuilder().heap(size, MemoryUnit.MB))
                                 .build())
                 .build(true);
     }
 
-    private Cache<URL, AudioStreamCache> getCache() {
-        return cacheManager.getCache("preConfigured", URL.class, AudioStreamCache.class);
+    private Cache<URL, ByteStreamCache> getCache() {
+        return cacheManager.getCache("preConfigured", URL.class, ByteStreamCache.class);
     }
 
     public InputStream getStream(URL uri) throws IOException {
-        Cache<URL, AudioStreamCache> cache = getCache();
+        Cache<URL, ByteStreamCache> cache = getCache();
 
-        AudioStreamCache stream = cache.get(uri);
+        ByteStreamCache stream = cache.get(uri);
         if (stream == null) {
-            stream = new AudioStreamCache(uri);
-            AudioStreamCache exists = cache.putIfAbsent(uri, stream);
+            stream = new ByteStreamCache();
+            ByteStreamCache exists = cache.putIfAbsent(uri, stream);
             if (exists != null) {
                 stream = exists;
             }
         }
-        return new ByteArrayInputStream(stream.getBytes());
+        return new ByteArrayInputStream(stream.getBytes(uri));
     }
 
-    private static class AudioStreamCache {
-
-        private URL uri;
+    private static class ByteStreamCache {
 
         private Lock lock = new ReentrantLock();
 
         private volatile byte[] bytes;
 
-        public AudioStreamCache(URL uri) {
-            this.uri = uri;
-        }
-
-        public byte[] getBytes() throws IOException {
+        public byte[] getBytes(URL uri) throws IOException {
             if (bytes == null) {
                 lock.lock();
                 try {
