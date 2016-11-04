19ee6a5587e06ca2dddb77ad57124f4fce8f1a20
hrosa <hdavidrosa@gmail.com>
2016-11-04 19:06:56 +0000

    #286 Patched start-mediaserver.sh script to be used in conjunction with RestComm.

--- bootstrap/src/main/config/autoconfig/start-mediaserver.sh
+++ bootstrap/src/main/config/autoconfig/start-mediaserver.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
 ## Description: Starts Media Server with auto-configuration.
 ## Author     : Henrique Rosa (henrique.rosa@telestax.com)
+## Parameters : 1. path to conf file (optional)
 
 export MS_HOME=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-local override_conf=$1
 
 verifyDependencies() {
     source $MS_HOME/.autoconfig/verify-dependencies.sh
 }
 
 loadConfigurationParams() {
+    local override_conf=$1
+
     source $MS_HOME/mediaserver.conf
-    if [ -z $override_conf ]; then
+    if [ -n "$override_conf" ]; then
         source $override_conf
     fi
 }
@@ -26,12 +28,12 @@ startMediaServer() {
         echo '... already running a session named "mediaserver"! Aborted.'
         exit 1
     else
-        tmux new -s mediaserver -d bin/run.sh
+        tmux new -s mediaserver -d $MS_HOME/bin/run.sh
         echo '...RestComm Media Server started running on session named "mediaserver"!'
     fi
 }
 
 verifyDependencies
-loadConfigurationParams
+loadConfigurationParams $1
 configureMediaServer
 startMediaServer
