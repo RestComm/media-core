bccbcb3f65745910825b7f503934f147faf91a4f
hrosa <hdavidrosa@gmail.com>
2017-03-06 23:06:06 +0000

    #392 Renamed packages in codecs/l16 module.

--- codecs/l16/src/main/java/org/restcomm/media/codec/l16/Encoder.java
+++ codecs/l16/src/main/java/org/restcomm/media/codec/l16/Encoder.java
@@ -20,7 +20,7 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
-package org.mobicents.media.server.impl.dsp.audio.l16;
+package org.restcomm.media.codec.l16;
 
 import org.mobicents.media.server.spi.dsp.Codec;
 import org.mobicents.media.server.spi.format.Format;
