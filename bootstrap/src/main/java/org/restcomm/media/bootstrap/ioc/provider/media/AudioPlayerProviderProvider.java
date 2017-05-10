4dbb7e57105d835018fe4a17efa1d115cf9af631
hrosa <hdavidrosa@gmail.com>
2017-05-10 17:42:12 +0100

    #437 AudioPlayerProvider now injects a DSP into the AudioPlayers it produces. Transcoding is now possible.

--- bootstrap/src/main/java/org/restcomm/media/bootstrap/ioc/provider/media/AudioPlayerProviderProvider.java
+++ bootstrap/src/main/java/org/restcomm/media/bootstrap/ioc/provider/media/AudioPlayerProviderProvider.java
@@ -24,6 +24,7 @@ package org.restcomm.media.bootstrap.ioc.provider.media;
 import org.restcomm.media.resource.player.audio.AudioPlayerProvider;
 import org.restcomm.media.resource.player.audio.RemoteStreamProvider;
 import org.restcomm.media.scheduler.PriorityQueueScheduler;
+import org.restcomm.media.spi.dsp.DspFactory;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
@@ -36,17 +37,19 @@ public class AudioPlayerProviderProvider implements Provider<AudioPlayerProvider
 
     private final PriorityQueueScheduler scheduler;
     private final RemoteStreamProvider streamProvider;
+    private final DspFactory dsp;
 
     @Inject
-    public AudioPlayerProviderProvider(PriorityQueueScheduler scheduler, RemoteStreamProvider streamProvider) {
+    public AudioPlayerProviderProvider(PriorityQueueScheduler scheduler, RemoteStreamProvider streamProvider, DspFactory dsp) {
         super();
         this.scheduler = scheduler;
         this.streamProvider = streamProvider;
+        this.dsp = dsp;
     }
 
     @Override
     public AudioPlayerProvider get() {
-        return new AudioPlayerProvider(scheduler, streamProvider);
+        return new AudioPlayerProvider(scheduler, streamProvider, dsp);
     }
 
 }
