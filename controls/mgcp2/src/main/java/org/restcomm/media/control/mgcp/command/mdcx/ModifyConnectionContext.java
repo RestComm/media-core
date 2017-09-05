de65fb5296c455497701c421711e4d1b2b1573b5
hrosa <hdavidrosa@gmail.com>
2017-09-05 16:41:43 +0100

    #466 Added Context POJO and core enums for MDCX Command.
    Implemented action to validate parameters.

---
+++ controls/mgcp2/src/main/java/org/restcomm/media/control/mgcp/command/mdcx/ModifyConnectionContext.java
@@ -0,0 +1,146 @@
+/*
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2017, Telestax Inc and individual contributors
+ * by the @authors tag. 
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.restcomm.media.control.mgcp.command.mdcx;
+
+import org.restcomm.media.control.mgcp.command.MgcpCommandParameters;
+import org.restcomm.media.control.mgcp.command.MgcpCommandResult;
+import org.restcomm.media.control.mgcp.endpoint.MgcpEndpointManager;
+import org.restcomm.media.spi.ConnectionMode;
+
+import com.google.common.util.concurrent.FutureCallback;
+
+/**
+ * @author Henrique Rosa (henrique.rosa@telestax.com)
+ *
+ */
+public class ModifyConnectionContext {
+
+    // Dependencies
+    private final MgcpEndpointManager endpointManager;
+
+    // Command Status
+    private final int transactionId;
+    private final MgcpCommandParameters parameters;
+
+    private int callId;
+    private String endpointId;
+    private int connectionId;
+    private ConnectionMode mode;
+    private String remoteDescription;
+    private String localDescription;
+
+    private FutureCallback<MgcpCommandResult> callback;
+    private Throwable error;
+
+    public ModifyConnectionContext(int transactionId, MgcpCommandParameters parameters, MgcpEndpointManager endpointManager) {
+        // Dependencies
+        this.endpointManager = endpointManager;
+
+        // Command Status
+        this.transactionId = transactionId;
+        this.parameters = parameters;
+
+        this.callId = -1;
+        this.endpointId = "";
+        this.connectionId = -1;
+        this.remoteDescription = "";
+        this.localDescription = "";
+        this.mode = null;
+    }
+
+    public MgcpEndpointManager getEndpointManager() {
+        return this.endpointManager;
+    }
+
+    protected int getTransactionId() {
+        return transactionId;
+    }
+
+    protected MgcpCommandParameters getParameters() {
+        return parameters;
+    }
+
+    protected int getCallId() {
+        return callId;
+    }
+
+    protected void setCallId(int callId) {
+        this.callId = callId;
+    }
+
+    protected String getEndpointId() {
+        return endpointId;
+    }
+
+    protected void setEndpointId(String endpointId) {
+        this.endpointId = endpointId;
+    }
+
+    protected int getConnectionId() {
+        return connectionId;
+    }
+
+    protected void setConnectionId(int connectionId) {
+        this.connectionId = connectionId;
+    }
+
+    protected ConnectionMode getMode() {
+        return mode;
+    }
+
+    protected void setMode(ConnectionMode mode) {
+        this.mode = mode;
+    }
+
+    protected String getRemoteDescription() {
+        return remoteDescription;
+    }
+
+    protected void setRemoteDescription(String remoteDescription) {
+        this.remoteDescription = remoteDescription;
+    }
+
+    protected String getLocalDescription() {
+        return localDescription;
+    }
+
+    protected void setLocalDescription(String localDescription) {
+        this.localDescription = localDescription;
+    }
+
+    protected FutureCallback<MgcpCommandResult> getCallback() {
+        return callback;
+    }
+
+    protected void setCallback(FutureCallback<MgcpCommandResult> callback) {
+        this.callback = callback;
+    }
+
+    protected Throwable getError() {
+        return error;
+    }
+
+    protected void setError(Throwable error) {
+        this.error = error;
+    }
+
+}
