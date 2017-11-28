b3a4ac772cead95e8df471d556d6bd79b6c1900f
hrosa <hdavidrosa@gmail.com>
2017-11-28 16:36:02 +0000

    #691 MGCP Endpoint FSM now recovers from unexpected exceptions.
    MGCP Endpoint now warns callbacks of denied transitions.

--- controls/mgcp2/src/main/java/org/restcomm/media/control/mgcp/endpoint/MgcpEndpointFsmImpl.java
+++ controls/mgcp2/src/main/java/org/restcomm/media/control/mgcp/endpoint/MgcpEndpointFsmImpl.java
@@ -21,21 +21,46 @@
 
 package org.restcomm.media.control.mgcp.endpoint;
 
+import com.google.common.util.concurrent.FutureCallback;
+import org.apache.log4j.Logger;
+import org.squirrelframework.foundation.fsm.StateMachineStatus;
 import org.squirrelframework.foundation.fsm.impl.AbstractStateMachine;
 
 /**
  * @author Henrique Rosa (henrique.rosa@telestax.com)
- *
  */
-public class MgcpEndpointFsmImpl
-        extends AbstractStateMachine<MgcpEndpointFsm, MgcpEndpointState, MgcpEndpointEvent, MgcpEndpointTransitionContext>
-        implements MgcpEndpointFsm {
+public class MgcpEndpointFsmImpl extends AbstractStateMachine<MgcpEndpointFsm, MgcpEndpointState, MgcpEndpointEvent, MgcpEndpointTransitionContext> implements MgcpEndpointFsm {
+
+    private static final Logger log = Logger.getLogger(MgcpEndpointFsmImpl.class);
 
     private final MgcpEndpointContext context;
 
-    // TODO Add listener for rollback in Exception
+    @Override
+    protected void afterTransitionCausedException(MgcpEndpointState fromState, MgcpEndpointState toState, MgcpEndpointEvent event, MgcpEndpointTransitionContext context) {
+        final Throwable t = this.getLastException().getTargetException();
 
-    // TODO Add listener when transition is denied
+        log.error("Endpoint " + this.context.getEndpointId() + " caught unexpected error.", t);
+
+        // Recover from error
+        this.setStatus(StateMachineStatus.IDLE);
+
+        // Warn callback that operation failed
+        final FutureCallback callback = context.get(MgcpEndpointParameter.CALLBACK, FutureCallback.class);
+        if (callback != null) {
+            callback.onFailure(t);
+        }
+    }
+
+    @Override
+    protected void afterTransitionDeclined(MgcpEndpointState fromState, MgcpEndpointEvent event, MgcpEndpointTransitionContext context) {
+        final FutureCallback<?> callback = context.get(MgcpEndpointParameter.CALLBACK, FutureCallback.class);
+
+        if (callback != null) {
+            // Warn callback that requested operation failed
+            final IllegalStateException error = new IllegalStateException("Operation " + event.name() + " not allowed on state " + fromState);
+            callback.onFailure(error);
+        }
+    }
 
     public MgcpEndpointFsmImpl(MgcpEndpointContext context) {
         super();
