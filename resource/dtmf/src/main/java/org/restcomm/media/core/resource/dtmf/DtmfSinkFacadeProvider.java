b5904f391aaac7980350a74d133936daeb764a79
Vladimir Morosev <morosev@gmail.com>
2018-04-17 23:19:38 +0200

    RMS-72 Sink Facade Provider implementation

--- resource/dtmf/src/main/java/org/restcomm/media/core/resource/dtmf/DtmfSinkFacadeProvider.java
+++ resource/dtmf/src/main/java/org/restcomm/media/core/resource/dtmf/DtmfSinkFacadeProvider.java
@@ -21,13 +21,43 @@
 
 package org.restcomm.media.core.resource.dtmf;
 
+import org.restcomm.media.core.component.audio.AudioOutput;
+import org.restcomm.media.core.component.oob.OOBOutput;
+import org.restcomm.media.core.resource.dtmf.DtmfDetectorProvider;
+import org.restcomm.media.core.resource.dtmf.DtmfSinkFacade;
+import org.restcomm.media.core.resource.dtmf.DtmfSinkFacadeProvider;
+import org.restcomm.media.core.resource.dtmf.InbandDtmfSink;
+import org.restcomm.media.core.resource.dtmf.Rfc2833DtmfSink;
+import org.restcomm.media.core.resource.dtmf.Rfc2833DtmfDetector;
+import org.restcomm.media.core.scheduler.PriorityQueueScheduler;
+import org.restcomm.media.core.spi.ComponentType;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Provides instances of {@link DtmfSinkFacade}
  *
  * @author Vladimir Morosev (vladimir.morosev@telestax.com) created on 13/04/2018
  */
-public interface DtmfSinkFacadeProvider {
+public class DtmfSinkFacadeProvider {
 
-    DtmfSinkFacade provide();
+    private static final AtomicInteger detectorId = new AtomicInteger(0);
+
+    private PriorityQueueScheduler scheduler;
+    private DtmfDetectorProvider detectorProvider;
+
+    public DtmfSinkFacadeProvider(PriorityQueueScheduler scheduler, DtmfDetectorProvider detectorProvider) {
+        this.scheduler = scheduler;
+        this.detectorProvider = detectorProvider;
+    }
+
+    public DtmfSinkFacade provide() {
+        AudioOutput output = new AudioOutput(scheduler, ComponentType.DTMF_DETECTOR.getType());
+        OOBOutput oobOutput = new OOBOutput(scheduler, ComponentType.DTMF_DETECTOR.getType());
+        int id = detectorId.getAndIncrement();
+        InbandDtmfSink inbandSink = new InbandDtmfSink("inband-dtmf-sink-" + id, detectorProvider.provide(), output);
+        Rfc2833DtmfSink oobSink = new Rfc2833DtmfSink("oob-dtmf-sink-" + id, new Rfc2833DtmfDetector(500), oobOutput);
+        return new DtmfSinkFacade(inbandSink, oobSink);
+    }
 
 }
